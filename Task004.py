b68b0701381fe6269342a0a332d499797aa4fd4e
Anna <annkrv80@yandex.ru>
2022-10-14 23:59:30 +0700

    формиромание многочлена натуральной степени k и случайными коэффициентами

--- Task004.py
+++ Task004.py
@@ -1,49 +1,39 @@
 #Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100)
 #многочлена и записать в файл многочлен степени k.
-import itertools
 import os
 from random import randint
-def clear(): return os.system('cls')
-
-
+clear=lambda:os.system('cls')
 clear()
+
+def create_coefficients(n):
+    coef = [randint(0, 100) for i in range(n + 1)]
+    return coef
+
+def create_polymom(coeffic):
+       k = len(coeffic)-1
+       polynom = ' '
+       for i in range(0,len(coeffic)):
+              if i == len(coeffic)-1:
+                     polynom+=f'{coeffic[i]}'
+              elif k==1:
+                     polynom+=f'{coeffic[i]}X + '
+              else:
+                     polynom+=f'{coeffic[i]}X^{k} + '
+              k-=1
+       polynom+=' =0'
+       return polynom
+
+
 k = int(input('Введите натуральную степень k '))
-coefficients = [randint(0, 100) for i in range(k + 1)]
-print(coefficients)
-
-
-def create_polynom(sp):
-    list = sp[::-1]
-    polynom = ''
-    if len(list) < 1:
-        polynom = 'x = 0'
-    else:
-        for i in range(len(list)):
-            if i != len(list) - 1 and list[i] != 0 and i != len(list) - 2:
-                polynom += f'{list[i]}x^{len(list)-i-1}'
-                if list[i+1] != 0:
-                    polynom += ' + '
-            elif i == len(list) - 2 and list[i] != 0:
-                polynom += f'{list[i]}x'
-                if list[i+1] != 0:
-                    polynom += ' + '
-            elif i == len(list) - 1 and list[i] != 0:
-                polynom += f'{list[i]} = 0'
-            elif i == len(list) - 1 and list[i] == 0:
-                polynom += ' = 0'
-    return polynom
-
-
-polynom1 = create_polynom(coefficients)
+coefficients1 = create_coefficients(k)
+polynom1 = create_polymom(coefficients1)
 print(polynom1)
-
-k = int(input('Введите натуральную степень k '))
-coefficients = [randint(0, 100) for i in range(k + 1)]
-print(coefficients)
-polynom2 = create_polynom(coefficients)
+coefficients2 = create_coefficients(k)
+polynom2 = create_polymom(coefficients2)
 print(polynom2)
 
-with open('file1.txt', 'w') as data:
-    data.write(polynom1)
-with open('file2.txt', 'w') as data:
-    data.write(polynom2)
+with open ('file1.txt','w') as data:
+       data.write(polynom1)
+with open ('file2.txt','w') as data:
+       data.write(polynom2)
+
